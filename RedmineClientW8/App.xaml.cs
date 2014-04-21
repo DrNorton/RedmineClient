e61162f0eeccecf157bdc9bfd63ef10472f124bf
dimaivanov1@mail.ru <rianon>
2014-04-22 00:45:58 +0400

    Init

---
+++ RedmineClientW8/App.xaml.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using Windows.ApplicationModel;
+using Windows.ApplicationModel.Activation;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+// The Blank Application template is documented at http://go.microsoft.com/fwlink/?LinkId=234227
+using Caliburn.Micro;
+using RedmineClientW8.Views;
+
+namespace RedmineClientW8
+{
+    public sealed partial class App
+    {
+        private WinRTContainer container;
+
+        public App()
+        {
+            InitializeComponent();
+        }
+
+        protected override void Configure()
+        {
+            container = new WinRTContainer();
+            container.RegisterWinRTServices();
+
+            //TODO: Register your view models at the container
+        }
+
+        protected override object GetInstance(Type service, string key)
+        {
+            var instance = container.GetInstance(service, key);
+            if (instance != null)
+                return instance;
+            throw new Exception("Could not locate any instances.");
+        }
+
+        protected override IEnumerable<object> GetAllInstances(Type service)
+        {
+            return container.GetAllInstances(service);
+        }
+
+        protected override void BuildUp(object instance)
+        {
+            container.BuildUp(instance);
+        }
+
+        protected override void PrepareViewFirst(Frame rootFrame)
+        {
+            container.RegisterNavigationService(rootFrame);
+        }
+
+        protected override void OnLaunched(LaunchActivatedEventArgs args)
+        {
+            DisplayRootView<MainView>();
+        }
+    }
+}
